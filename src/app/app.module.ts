b1c8bc01810c5ef230faf2a7b0252aa7ab876154
Iraj Mashar <i.mashar@uniccomp.de>
2020-09-25 15:40:40 +0200

    added animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { HoverContainerComponent } from './hover-container/hover-container.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HomeComponent, HoverContainerComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Waifu } from "../waifu";
 import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes,
+} from "@angular/animations";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
+  animations: [
+    trigger("myAnimation", [
+      state("small", style({ transform: "scale(1)", boxShadow: "0px 3px 12px 0px rgba(0,0,0,0.75)" })),
+      state("large", style({ transform: "scale(1.1)", boxShadow:"0px 6px 25px 0px rgba(0,0,0,0.75)"})),
+      transition("small <=> large", animate("50ms ease-in")),
+    ]),
+    trigger("myToggleInfoTrigger", [
+      transition(":enter", [
+        style({opacity: 0}),
+        animate('100ms ease-in', style({opacity: 1}))
+      ]),
+      transition(":leave", [
+        animate('100ms ease-in', style({opacity: 0}))
+        ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   waifu: Waifu = {
@@ -13,7 +37,26 @@ export class HomeComponent implements OnInit {
     anime: "Darling in the Franxx",
   };
 
+  state: string = "small";
+
+  info: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  animateMeLarge(): void {
+    if(!(this.state === "large")){
+      this.state = "large"
+    }
+  }
+  
+  animateMeSmall(): void {
+    this.state = "small"
+  }
+
+  toggleInfo(): void {
+    this.info = !this.info
+  }
+
 }
